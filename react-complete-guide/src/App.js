a334a3c5928c357f0eb18b7df82d4847cdb0050a
acparkgh <acpark123@gmail.com>
2021-01-26 17:09:47 -0500

    using css modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 // import styled from 'styled-components';
 // import Radium, {StyleRoot} from 'radium';
@@ -105,22 +106,22 @@ class App extends Component {
       }
     }
     
-    const classes = [];
+    const Assignedclasses = [];
 
     if(this.state.persons.length <=2) {
-      classes.push('red');
+      Assignedclasses.push(classes.red);
     }
 
     if(this.state.persons.length <=1){
-      classes.push('bold');  
+      Assignedclasses.push(classes.bold);  
     }
     
     return (
       
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I am a React App</h1>
-          <p className={classes.join(' ')} >This is really working!</p>
-          <button className="button" >
+          <p className={Assignedclasses.join(' ')} >This is really working!</p>
+          <button className="classes.Button" >
             Show Persons
           </button>
           {persons}
